8cdc6c38b3a55fda37892a88b470a0927db11e20
Clemens Siebler <clemens.siebler@gmail.com>
2020-10-29 09:23:09 +0100

    Also updated deployment code

--- devops-deploy-pipeline-with-tests/add_to_pipeline_endpoint.py
+++ devops-deploy-pipeline-with-tests/add_to_pipeline_endpoint.py
@@ -19,14 +19,11 @@ print(f'Pipeline ID: {pipeline_id}')
 published_pipeline = PublishedPipeline.get(workspace=ws, id=pipeline_id)
 print(f'Published Pipeline: {published_pipeline}')
 
-# Check if PipelineEndpoint already exists
-if any(pe.name == endpoint_name for pe in PipelineEndpoint.list(ws)):
+try:
     print(f'Pipeline Endpoint with name {endpoint_name} already exists, will add pipeline to it')
     pipeline_endpoint = PipelineEndpoint.get(workspace=ws, name=endpoint_name)
-    pipeline_endpoint.add(published_pipeline)
-    # Set it to default, as we already tested it beforehand!
-    pipeline_endpoint.set_default(published_pipeline)
-else:
+    pipeline_endpoint.add_default(published_pipeline)
+except Exception:
     print(f'Will create Pipeline Endpoint with name {endpoint_name}')
     pipeline_endpoint = PipelineEndpoint.publish(workspace=ws,
                                                 name=endpoint_name,
