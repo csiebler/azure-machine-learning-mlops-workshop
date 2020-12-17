dc45473f1eb06e9f5622f9e45a06f019a0de451a
Clemens Siebler <csiebler@users.noreply.github.com>
2020-12-17 16:46:05 +0100

    Added Hyperdrive Examples (#2)
    * Added Hyperdrive Example via CLI
    
    * Updated readme
    
    * Added hyperdrive notebooks

---
+++ pipelines-hyperdrive-step/README.md
@@ -0,0 +1,27 @@
+# Exercise Instructions
+
+Open [`hyperdrive_pipeline.ipynb`](hyperdrive_pipeline.ipynb) and follow the instructions in the notebook.
+
+# Running this via CLI
+
+You can also run the Hyperdrive Hyperparameter Tuning via CLI:
+
+```console
+az ml folder attach -w <YOUR WORKSPACE NAME> -g <YOUR RESOURCE GROUP>
+az ml run submit-hyperdrive --hyperdrive-configuration-name hyperdrive_config.yml -c hyperdrive -e hyperdrive-test
+```
+
+In this case:
+* [`hyperdrive_config.yml`](hyperdrive_config.yml) holds the configuration for the hyperparameter tuning. Full details on the parameters can be found [here](https://docs.microsoft.com/en-us/azure/machine-learning/how-to-tune-hyperparameters#define-the-search-space)
+* [`hyperdrive.runconfig`](hyperdrive.runconfig) holds the general script definition (which dataset, cluster, etc.)
+* [`train.py`](train.py) takes all the hyperparameters as argument inputs
+
+You can check the results in the Studio UI (navigate to the run, then select `Child Runs`):
+
+![Hyperdrive in Studio UI](../media/hyperdrive_example.png)
+
+Each Hyperparameter permutation is its own child run.
+
+# Knowledge Check
+
+To be written
---
+++ pipelines-hyperdrive-step/register.py
@@ -0,0 +1,41 @@
+import json
+import os
+import ast
+import argparse
+import azureml.core
+from azureml.core import Run
+from azureml.pipeline.steps.hyper_drive_step import HyperDriveStepRun
+
+def getRuntimeArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', type=str)
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = getRuntimeArgs()
+    model_name = args.model_name
+    
+    # current run is the registration step
+    current_run = Run.get_context()
+
+    # parent run is the overall pipeline
+    parent_run = current_run.parent
+
+    # Get the HyperDriveStep of the pipeline by name (make sure only 1 exists)
+    hd_step_run = HyperDriveStepRun(step_run=parent_run.find_step_run('hyperparameter-tuning')[0])
+
+    # Get RunID for best run
+    best_run = hd_step_run.get_best_run_by_primary_metric()
+    best_run_id = best_run.id
+
+    # Get the best run's metrics and hyperparameters
+    hyperparameters = ast.literal_eval(hd_step_run.get_hyperparameters()[best_run_id].replace('--', ''))
+    metrics = hd_step_run.get_metrics()[best_run_id]
+
+    best_run.register_model(model_path='outputs/model.pkl',
+                            model_name=model_name,
+                            properties={**metrics, **hyperparameters})
+
+if __name__ == "__main__":
+    main()
