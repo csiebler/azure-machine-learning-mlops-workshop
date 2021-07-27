22f7c44c43242034fa4aceb591c2e43b4684beec
Clemens Siebler <clemens.siebler@gmail.com>
2021-07-27 12:50:43 +0200

    Added dataset registration

--- pipelines-multi-step-pipeline/register.py
+++ pipelines-multi-step-pipeline/register.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from azureml.core import Run
+from azureml.core import Run, Dataset
 from azureml.pipeline.core import PipelineRun
 
 parser = argparse.ArgumentParser()
@@ -12,6 +12,7 @@ print(f'Arguments: {args}')
 
 # current run is the registration step
 current_run = Run.get_context()
+ws = current_run.experiment.workspace
 
 # parent run is the overall pipeline
 parent_run = current_run.parent
@@ -22,5 +23,19 @@ pipeline_run = PipelineRun(parent_run.experiment, parent_run.id)
 training_run = pipeline_run.find_step_run('train-step')[0]
 print(f'Training run: {training_run}')
 
+prepare_run = pipeline_run.find_step_run('prepare-step')[0]
+print(f'Prepare run: {prepare_run}')
+
+# Retrieve input training datasets
+datasets = []
+details = prepare_run.get_details()
+for d in details['inputDatasets']:
+    print(f'Dataset info: {d}')
+    name = d['consumptionDetails']['inputName']
+    dataset = d['dataset']
+    datasets.append([name, dataset])
+
+# Register model
 model = training_run.register_model(model_name=args.model_name,
-                                    model_path=args.model_path)
+                                    model_path=args.model_path,
+                                    datasets=datasets)
